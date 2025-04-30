f6558f0f65f53bb97b253afd2116c4dad2ae1428
n0llid <n0llid@protonmail.com>
2025-04-30 00:12:46 -1000

    init.lua cleaned, plugins moved to plugin folder

--- init.lua
+++ init.lua
@@ -13,38 +13,8 @@ require 'vim-options' -- file located at 'nvim/lua/vim-options.lua'
 
 -- [[ Configure and install plugins ]]
 -- TODO: Modularize plugins and move them to their own separate files.
-require('lazy').setup({
-  { import = "plugins" },
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+require('lazy').setup({ { import = "plugins" } })
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
-  --
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-})
 -- TODO: Consolidate/fix lsp configs to follow one standard
 -- I think this was auto added from mason
 require('lspconfig').harper_ls.setup {}

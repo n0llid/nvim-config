1d49bef0645fe7f65c5db872db338447cc027c06
n0llid <n0llid@protonmail.com>
2025-11-07 13:02:52 -1000

    Plugin Remove: speed.nvim removed

---
+++ lua/plugins/restoration.nvim.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "paradoxical-dev/restoration.nvim",
+    event = "VeryLazy", -- optional but recommended
+    opts = {
+      -- optional config here
+    }
+  },
+}
--- lua/plugins/speed.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    -- ':Speed toggle' to toggle the plugin
-    "Hashino/speed.nvim",
-    opts = {},
-  }
-}
